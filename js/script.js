d26ebcdbef60773452c47825af01d0872c968b5f
Sasha-OK-bit <sasha.vorontsov@stu.ocsb.ca>
2025-06-09 17:04:42 +0000

    Added favicons and changed titles and descriptions

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Sasha Vorontsov
-// Created on: May 2025
+// Created on: June 2025
 // This file contains the JS functions for index.html
 
 "use strict"
 
   if (navigator.serviceWorker) {
-+   navigator.serviceWorker.register("/ICD2O-Unit-6-03-Sasha-OK-bit/sw.js",
++   navigator.serviceWorker.register("/ICD2O-Assignment-Sasha-OK-bit/sw.js",
   {
   scope: "/ICD2O-Unit-6-03-Sasha-OK-bit/",
     })
