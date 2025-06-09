ee02a097b3e87c0a6e19fe90e1209ed79d882740
Sasha-OK-bit <sasha.vorontsov@stu.ocsb.ca>
2025-06-09 16:33:18 +0000

    Added pokemon api website and removed image api

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Sasha Vorontsov
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+  if (navigator.serviceWorker) {
++   navigator.serviceWorker.register("/ICD2O-Unit-6-03-Sasha-OK-bit/sw.js",
+  {
+  scope: "/ICD2O-Unit-6-03-Sasha-OK-bit/",
+    })
+}
+
+async function getData() {
+  try {
+    const url = "https://pokeapi.co/api/v2/pokemon/ditto"
+    const infoJSON = await fetch(url);
+    const JSONdata = await infoJSON.json();
+
+    const pokemon = JSONdata.main.temp;
+
+    document.getElementById("answer1").innerHTML = pokemon.toFixed(0);
+  }catch (error) {
+    console.log(error);
+  }
+}
